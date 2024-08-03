3629b7568c85e939847054425ecc753ac0fd7e22
IlhamNur <romdhoninuril@gmail.com>
2024-08-03 22:04:30 +0700

    make login and register

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class LoginController extends Controller
+{
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $this->validator($request->all())->validate();
+
+        if (Auth::attempt($request->only('email', 'password'))) {
+            return redirect()->route('home');
+        }
+
+        return redirect()->back()->withErrors(['email' => 'Invalid credentials.']);
+    }
+
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'password' => ['required', 'string', 'min:8'],
+        ]);
+    }
+}
