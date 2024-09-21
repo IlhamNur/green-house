c2704e971d45ed24f6d01c84860566ad38554614
IlhamNur <romdhoninuril@gmail.com>
2024-09-21 18:24:16 +0700

    edit miss in plant list view

--- resources/views/plant-list.blade.php
+++ resources/views/plant-list.blade.php
@@ -27,7 +27,7 @@
       content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
     />
 
-    <title>Plant List| SmartGrow</title>
+    <title>Plant List | SmartGrow</title>
 
     <meta name="description" content="" />
 
